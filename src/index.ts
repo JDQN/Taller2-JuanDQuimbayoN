c3a89befe682341d2e1c9fb4b5aa374c0453b7c6
Edwin Sanchez <esanchez1988@gmail.com>
2023-04-12 20:11:55 -0500

    Ajustes del index.html y crear evento click del form

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-function crearAlerta(){
-    alert("Hola Mundo");
-}
+const form = document.getElementById('form') as HTMLFormElement;
+const btnGuardar = document.getElementById('btn-guardar') as HTMLButtonElement;
+
+btnGuardar.addEventListener('click', (e) => {
+    //e.preventDefault();
+    console.log("Ingreso a la validación del formulario");   
+
+    //Obtener los valores de los inputs
+    const tipoDocumento = document.getElementById('tipoDocumento') as HTMLInputElement;
+    const numeroDocumento = document.getElementById('numeroDocumento') as HTMLInputElement;
+    const nombre = document.getElementById('nombre') as HTMLInputElement;
+    const apellido = document.getElementById('apellido') as HTMLInputElement;
+    const celular = document.getElementById('celular') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const linkedin = document.getElementById('linkedin') as HTMLInputElement;
+    const github = document.getElementById('github') as HTMLInputElement;
+
+
+    //La validación del formulario
+    console.log(tipoDocumento.value);
+
+});
